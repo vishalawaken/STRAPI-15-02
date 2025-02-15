98ac594bdf1277dae9a2064263e0b5593469d055
vishalawaken <tsinghvishall@gmail.com>
2025-02-15 22:14:49 +0530

    Fix: Add Rollup dependencies for Linux

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 export NODE_ENV=production
 export SWCRC=true
+
+# Remove existing dependencies
+rm -rf node_modules
+rm -f package-lock.json
+
+# Install dependencies
 npm install @swc/core
 npm install @swc/core-linux-x64-gnu
+npm install @rollup/rollup-linux-x64-gnu
 npm install
 npm run build
