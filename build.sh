3c6541bb9a7e0b6d20ca666b626d71b6aa97fde5
vishalawaken <tsinghvishall@gmail.com>
2025-02-15 22:13:07 +0530

    Fix: Update SWC dependencies for glibc

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export NODE_ENV=production
+export SWCRC=true
 npm install @swc/core
 npm install @swc/core-linux-x64-gnu
-npm install @swc/core-linux-x64-musl
 npm install
 npm run build
